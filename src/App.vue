6cf29e4e559024dd440f8384ba0600e64ef1684c
TheMoonVyy <TheMoonVyy@gmail.com>
2025-03-28 09:46:33 +0530

    fix text

--- src/App.vue
+++ src/App.vue
@@ -12,7 +12,7 @@ const value = ref(4)
     <div class="bg-white dark:bg-gray-800 flex flex-col items-center justify-center gap-3 shadow-lg rounded-2xl p-6 w-96">
       <h2>Without any pt, works fine</h2>
       <InputNumber v-model="value" />
-      <h2>With pt:root:class="text-red", works fine</h2>
+      <h2>With pt:root:class="text-xl", works fine</h2>
       <InputNumber v-model="value" pt:root:class="text-xl" />
       <h2>with pt:pcInputText:root:class="text-xl", doesn't work</h2>
       <InputNumber v-model="value" pt:pcInputText:root:class="text-xl" />
